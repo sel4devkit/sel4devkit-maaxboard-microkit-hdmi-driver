1f890ad2f6a91c5e2872d110824084abff292320
tharveyy <tom.harvey@capgemini.com>
2024-08-09 14:15:27 +0100

    Modified build script

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ export MICROKIT_BOARD="maaxboard"
 export MICROKIT_CONFIG="debug"
 export BUILD_DIR="./build"
 export SOURCE_LOCATION=$(pwd)
+export MICROKIT_SDK="dep/microkit/out/sel4devkit-maaxboard-microkit-dev/out/microkit-sdk-1.2.6"
+export BOARD_DIR="$MICROKIT_SDK/board/maaxboard/debug/"
+export PYTHONPATH="$MICROKIT_SDK/bin"
+export MICROKIT_TOOL="$MICROKIT_SDK/bin"
 
 example_list=("empty_client" "moving_square" "resolution_change" "rotating_bars" "static_image") 
 
@@ -30,43 +34,6 @@ if [ $1 = "empty_client" ] ; then
 else
     export CURRENT_EXAMPLE="examples/$1"
 fi 
-
-
-# Use no second argument if using follwoing docker configuration:
-# https://github.com/sel4-cap/sel4devkit-maaxboard-docker-dev-env
-
-if [ -z "$2" ]; then
-
-    export MICROKIT_SDK="dep/microkit/out/microkit-sdk-1.2.6"
-    export BOARD_DIR="$MICROKIT_SDK/board/maaxboard/debug/"
-    export PYTHONPATH="$MICROKIT_SDK/bin"
-    export MICROKIT_TOOL=" $MICROKIT_SDK/bin/microkit"
-
-# Example of a custom configuration
-elif [ $2 = "c" ] ; then
-
-    export MICROKIT_DIR="$HOME/dev/microkit/mk-manifest/microkit"
-    export MICROKIT_SDK="$MICROKIT_DIR/release/microkit-sdk-1.2.6"
-    export PATH="$MICROKIT_DIR/../compiler/bin:$PATH"
-    
-    export BOARD_DIR="$MICROKIT_SDK/board/$MICROKIT_BOARD/$MICROKIT_CONFIG"
-    export PYTHONPATH="$MICROKIT_DIR/tool"
-    export MICROKIT_TOOL="python -m microkit"
-
-else
-    echo "Invalid argument - Leave empty if building inside docker container, and 'c' if using own configuration"
-    exit 1
-fi
-
-
-# Confirm picolibc files exist
-if  test -e dep/picolibc/libc.a  &&  test -e dep/picolibc/picolibc.specs ; then
-    echo "Found picolib build files"
-else
-    echo "Can't find picolib build files. Place libc.a and picolib.specs from your picolib build into the picolib directory.
-Alternatively run the picolib/build-picolib.sh script to build picolib."
-    exit
-fi
 
 
 # Make clean 
